9173d3c42cbdcc7e47e06ee846273ee13bb13a66
hermodesign <hermodesign@gmail.com>
2023-12-12 18:41:51 +0100

    fin

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
   const submitButton = document.querySelector('.btn');
+  const num = document.querySelector('.num');
+
+  const container = document.querySelector('.container');
+  const ratingState = document.querySelector('.ratingState');
 
   submitButton.addEventListener('click', function () {
     const selectedRadioButton = document.querySelector(
@@ -9,6 +13,9 @@ document.addEventListener('DOMContentLoaded', function () {
     if (selectedRadioButton) {
       const selectedNumber = selectedRadioButton.nextElementSibling.textContent;
       console.log('Selektovani broj:', selectedNumber);
+      num.textContent = selectedNumber;
+      container.classList.add('hidden');
+      ratingState.classList.remove('hidden');
     } else {
       console.log('Nijedan radio dugme nije selektovan.');
     }
